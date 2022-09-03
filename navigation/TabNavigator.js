b32bc94dea0dea79963932c5afb566b0435a9686
Ashu12325 <84065806+Ashu12325@users.noreply.github.com>
2022-09-03 15:10:15 +0530

    Add files via upload

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
+
+import Feed from "../screens/Feed";
+import NewPost from "../screens/NewPost";
+const Tab = createMaterialBottomTabNavigator();
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      labeled={false}
+      barStyle={styles.bottomTabStyle}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Feed") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "New Post") {
+            iconName = focused ? "add-circle" : "add-circle-outline";
+          }
+          return (
+            <Ionicons
+              name={iconName}
+              size={RFValue(25)}
+              color={color}
+              style={styles.icons}
+            />
+          );
+        }
+      })}
+      activeColor={"#0099cc"}
+      inactiveColor={"gray"}
+    >
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="New Post" component={NewPost} />
+    </Tab.Navigator>
+  );
+};
+
+const styles = StyleSheet.create({
+  bottomTabStyle: {
+    backgroundColor: "white",
+    height: "8%",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    overflow: "hidden",
+    position: "absolute"
+  },
+  icons: {
+    width: RFValue(30),
+    height: RFValue(30)
+  }
+});
+
+export default BottomTabNavigator;
---
+++ screens/PostCard.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  Dimensions
+} from "react-native";
+
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+
+
+let customFonts = {
+  "Combo": require("../assets/fonts/Combo-Regular.ttf")
+};
+
+
+export default class PostCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />;
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text style={styles.appText}>Post Card!</Text>
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  appText:{
+    color:"black",
+    fontFamily:"Combo",
+  }
+});
